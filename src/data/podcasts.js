80364db7b7e7ab6e580e8856b3acfa2b1b4fef28
laurentnunez <lnunez@hotmail.fr>
2022-06-03 11:48:34 +0200

    update data

---
+++ src/data/podcasts.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    id: 1,
+    title: 'Un Coin de Ciné',
+    image: 'https://d3t3ozftmdmh3i.cloudfront.net/production/podcast_uploaded_nologo/19057193/19057193-1648472550842-a7ea2a84d72c6.jpg',
+  },
+  {
+    id: 2,
+    title: 'Culture Sport Club',
+    image: 'https://s3-us-west-2.amazonaws.com/anchor-generated-image-bank/production/podcast_uploaded_nologo400/23821423/23821423-1648932297599-ddf0284e6bf25.jpg',
+  },
+  {
+    id: 3,
+    title: 'Penser & Faire',
+    image: 'https://s3-us-west-2.amazonaws.com/anchor-generated-image-bank/production/podcast_uploaded_nologo400/20460142/20460142-1640269513011-0580e0411f991.jpg',
+  },
+];
